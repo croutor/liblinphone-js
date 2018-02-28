d8d1ae2d31de872affeec9ddb30195fd0933b124
Vincent Hervieux <vincent.hervieux@telecomsante.com>
2018-03-01 00:25:53 +0100

    first binding

--- registration.h
+++
@@ -1,9 +0,0 @@
-#ifndef REGISTRATION_H
-#define REGISTRATION_H
-
-#include "linphone/linphonecore.h"
-
-int registration(LinphoneCore *lc, char* identity, char* password);
-int unregistration(LinphoneCore *lc);
-
-#endif
